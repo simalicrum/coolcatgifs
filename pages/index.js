7c1c5c240759ec0d348e3e142b173809fe959100
Robert Reinert <robert.reinert@gmail.com>
2021-03-25 18:12:58 -0700

    margin fixes and footer

--- pages/index.js
+++ pages/index.js
@@ -13,17 +13,21 @@ export async function getServerSideProps() {
 
 export default function Home({catGifURL}) {
   return (
-    <div className="bg-purple-600 h-screen w-screen">
+    <div className="h-screen w-screen">
       <Head>
         <title>coolcatgifs.com</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="">Welcome to coolcatgifs.com</h1>
-      <div className="">
-        <Image src={catGifURL.images.original.url} height={catGifURL.images.original.height} width={catGifURL.images.original.width}></Image>
+      <div className="h-screen flex flex-col items-center justify-center m-4">
+        <div className="rounded-lg shadow-xl overflow-hidden" >
+        <img className="" src={catGifURL.images.original.url}></img>
+        </div>
       </div>
-      <footer className="fixed bottom-0">
-        <Image src="/Poweredby_100px-White_VertText.png" height="36px" width="100px"></Image>
+      <footer className="fixed w-screen bottom-0 flex justify-between">
+        <img className="h-8 m-4" src="/Poweredby_100px-White_VertText.png"></img>
+        <a href="https://www.github.com/simalicrum/coolcatgifs">
+          <img className="h-8 m-4 w-8 opacity-50" src="/Octicons-mark-github.svg"></img>
+        </a>
       </footer>
     </div>
   )
